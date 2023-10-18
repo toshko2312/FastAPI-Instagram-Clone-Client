c78f8b223c4bf97d15a18206c1ef895ee871e816
Todor Milushev <toshko_2312@abv.bg>
2023-10-18 19:23:54 +0300

    Added retrieve posts from backend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+
+const BASE_URL = 'http://localhost:8000/'
+
 function App() {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch(BASE_URL + 'post/all')
+    .then(response => {
+      const json = response.json()
+      console.log(json);
+      if (response.ok) {
+        return json
+      }
+      throw response
+    })
+    .then(data => {
+      setPosts(data)
+    })
+    .catch(error => {
+      console.log(error);
+      alert(error)
+    })
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    'Hello world'
   );
 }
 
