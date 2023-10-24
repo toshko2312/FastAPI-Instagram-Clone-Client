db4e7a1b066efbfd5fc4c4345e8260451559d873
Todor Milushev <toshko_2312@abv.bg>
2023-10-25 01:34:40 +0300

    Added caption and comments

--- src/post.js
+++ src/post.js
@@ -6,6 +6,7 @@ const BASE_URL = 'http://localhost:8000/'
 function Post({ post }) {
 
     const [imageUrl, setImageUrl] = useState("")
+    const [comments, setComments] = useState([])
 
     useEffect(() => {
         if (post.image_url_type === 'absolute') {
@@ -15,12 +16,27 @@ function Post({ post }) {
         }
     }, [])
 
+    useEffect(() => {
+        setComments(post.comments)
+    }, [])
+
     return (
         <div className='post'>
             <img
                 className="post_image"
                 src={imageUrl}
             />
+
+            <h4 className="post_text">{post.caption}</h4>
+            <div className="post_comments">
+                {
+                    comments.map((comment) => (
+                        <p>
+                            <strong>{comment.username}:</strong> {comment.text}
+                        </p>
+                    ))
+                }
+            </div>
         </div>
     )
 }
